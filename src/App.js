be5a6ec3510faac92c347ace8da7902cd889dd6d
nico_floriani <floriani.nico@gmail.com>
2023-08-30 10:00:47 -0700

    Fetched data from API, made use of statuses, passed data to UserDetails component, and added fetchData on request

--- src/App.js
+++ src/App.js
@@ -4,9 +4,35 @@ import { Loader } from "react-feather";
 import UserDetails from "./UserDetails";
 
 const App = () => {
+  const [data, setData] = useState({});
+  const [status, setStatus] = useState("loading"); // loading, success, error
+  const fetchData = async () => {
+    try {
+      setStatus("loading");
+      const request = await fetch("https://randomuser.me/api");
+      const { results } = await request.json();
+      if (request.status === 200) {
+        setData(results[0]);
+        setStatus("success");
+      }
+    } catch (e) {
+      console.log(e);
+      setStatus("error");
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <UserDetails />
+      {status === "success" && (
+        <UserDetails data={data} fetchData={fetchData} />
+      )}
+      {status === "loading" && <Loader />}
+      {status === "error" && (
+        <p>Something went wrong, please try again in a few minutes.</p>
+      )}
     </div>
   );
 };
